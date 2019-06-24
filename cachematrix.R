a49636fc6946ecae3892602d66f08bb0e808b8c5
wfrowe <wfrowe2@gmail.com>
2019-06-23 22:50:19 -0400

    R Programming Week 3 Assignment Initial Commit -makeCacheMatrix -cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## WFR R Programming Assignment 2
 
-## Write a short comment describing this function
+## Cache the inverse of a matrix
+
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## Set the value of the matrix
+    inv <- NULL    ## Start the inverse at null
+    set <- function(y) {
+        x <<- y  ## when set is called need to change the matrix to the new one
+        inv <<- NULL ##null out again
+    }
+    
+    ## get the value of the matrix
+    get <-function() x
+    
+    ## Set the value of the inverse
+    setinv <-function(inverse) inv <<- inverse  
+    
+    ## get the value of the inverse
+    getinv <- function() inv
+    
+    ## Create the object so cacheSolve can run onit
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    
+    ## make the inverse from the first function
+    inv <- x$getinv()
+    
+    ## if the inverse is already calculated because the matrix hasn't changed, return the cache
+    if(!is.null(inv)) {
+        message("Getting Cached Data")
+        return(inv)
+    }
+    
+    ## If we've gotten this far...
+    ## Define what 'data' is so we can do stuff to it (i.e. solve the matrix in the next line)
+    data <- x$get()
+    inv <- solve(data, ...)
+    
+    ## set the new inverse so makeCacheMatrix is up to data
+    x$setinv(inv)
+    
+    ##return the inverse
+    inv
 }
